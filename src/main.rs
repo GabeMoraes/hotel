1503604ed7426a5ca75ec77df5798b92f945eaa4
GabeMoraes <gabriel.mfxc@outlook.com>
2024-12-16 07:08:47 -0300

    feat!: complete CRUD operations

--- src/main.rs
+++ src/main.rs
@@ -218,6 +218,187 @@ fn display_guest() {
     println!("Payment method: {:?}", guest.pay_method);
 }
 
+fn update_guest() {
+    let mut delete_flag: bool = false;
+    let mut old_id: u64 = 0;
+
+    input_msg("id".to_string());
+
+    let mut id = String::new();
+    
+    io::stdin()
+        .read_line(&mut id)
+        .expect("Failed to read line.");
+
+    id = id.trim().to_string();
+
+    let path = format!("{}{}", id, ".bin");
+
+    let mut bin_data_from_file = Vec::new();
+    let mut file = File::open(path);
+    file.expect("REASON").read_to_end(&mut bin_data_from_file);
+
+    let mut guest: Guest = bincode::deserialize(&bin_data_from_file).unwrap();
+    
+    println!("Select field to change.");
+    println!("Please input an option:");
+    println!("1 - Name");
+    println!("2 - ID");
+    println!("3 - Birth");
+    println!("4 - Gender");
+    println!("5 - Address street");
+    println!("6 - Address number");
+    println!("7 - Postal code");
+    println!("8 - Telephone number");
+    println!("9 - Payment method");
+    
+    let mut input_line = String::new();
+
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+
+    if input_line.trim() == "1" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.name = input_line.trim().to_string();
+    }
+
+    else if input_line.trim() == "2" {
+        old_id = guest.id;
+
+        delete_flag = true;
+
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.id = input_line.trim().parse().expect("Not int");
+    }
+
+    else if input_line.trim() == "3" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.birth = input_line.trim().to_string();
+    }
+
+    else if input_line.trim() == "4" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        if input_line.trim() == "Masc" {
+            guest.gender = Gender::Masc;
+        }
+
+        else if input_line == "Fem" {
+            guest.gender == Gender::Fem;
+        }
+
+        else {
+            guest.gender == Gender::Nb;
+        }
+    }
+
+    else if input_line.trim() == "5" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.address_st = input_line.trim().to_string();
+    }
+
+    else if input_line.trim() == "6" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.address_n = input_line.trim().to_string();
+    }
+
+    else if input_line.trim() == "7" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.tel_number = input_line.trim().parse().expect("Not int");
+    }
+
+    else if input_line.trim() == "8" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        guest.tel_number = input_line.trim().to_string();
+    }
+
+    else if input_line.trim() == "9" {
+        let mut input_line = String::new();
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
+
+        if input_line.trim() == "Credit" {
+            guest.pay_method = PayMethod::Credit;
+        }
+
+        else if input_line == "Cash" {
+            guest.pay_method == PayMethod::Cash;
+        }
+    }
+
+    else {
+        println!("Illegal option.");
+        return;
+    }
+
+    println!("{:?}", guest);
+
+    let bin_data = bincode::serialize(&guest).unwrap();
+
+    let mut file = File::create(guest.id.to_string() + ".bin");
+    file.expect("REASON").write_all(&bin_data);
+    println!("Data saved to '{}.bin'.", guest.id);
+
+    if delete_flag {
+        // Removendo o arquivo
+        match fs::remove_file(old_id.to_string() + ".bin") {
+            Ok(_) => println!("Id {} changed", id),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+
+    // Lendo o arquivo binário.
+    let mut bin_data_from_file = Vec::new();
+    let mut file = File::open(guest.id.to_string() + ".bin");
+    file.expect("REASON").read_to_end(&mut bin_data_from_file);
+
+    // Desserialização: Recuperando a struct a partir dos dados binários.
+    let guest: Guest = bincode::deserialize(&bin_data_from_file).unwrap();
+    println!("Deserialized data: {:#?}", guest);
+}
+
 fn build_guest(
         name: String, id: u64, birth: String, gender: Gender,
         address_st: String, address_n: String, postal_code: u32,
@@ -241,6 +422,7 @@ fn show_menu() {
     println!("1 - Check in guest");
     println!("2 - Check out guest");
     println!("3 - Display guest");
+    println!("4 - Update guest");
     println!("\n0 - Exit");
 }
 
@@ -270,6 +452,10 @@ fn main() {
 
         else if input == "3\n" {
             display_guest();
+        }
+
+        else if input == "4\n" {
+            update_guest();
         }
     }
 }
